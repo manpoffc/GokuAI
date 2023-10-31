d88321eb4f64e6da766c61a1f058b6001240b510
Manpreet Singh <manpoffc@gmail.com>
2023-10-31 12:56:39 -0400

    Project Setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1 className="text-6xl text-orange-600">Dashboard Page</h1>
+    </div>
+  );
+}
